dbe234575a15aa7b740731c1a9c93e24aeb3c50e
DavronDostqobilov <dostqobilovdavron885@gamil.com>
2023-07-27 17:13:05 +0500

    third

---
+++ 9-dars/README.md
@@ -0,0 +1,8 @@
+# If else statement
+Else statement: If statement bilan bog'liq bo'ladi va shart rost bo'lmasa bajariladigan qo'shma buyruq blokini belgilaydi
+
+# Nested If statement
+Ichma ich If Else lardan foydalanish
+
+# Elif statement
+Else if statement: If statementdan so'ng keladi va bir nechta alternativ shartlar berish imkonini beradi. Shartlardan faqat biri bajariladi.
---
+++ 9-dars/main.py
@@ -0,0 +1,25 @@
+a1=21
+if a1%2==0:
+    print('Even number')
+else:
+    print('Odd number')
+
+# /////////////////////////////////////
+x = 10
+y = 5
+if x > y:
+    if x % y == 0:
+        print("x katta y dan va x y ga qoldiqsiz bo'linadi.")
+    else:
+        print("x katta y dan, lekin x y ga qoldiqsiz bo'linmaydi.")
+else:
+    print("x kichik y dan.")
+# /////////////////////////////////////
+a2=12
+if a2>0:
+    print("0 dan katta")
+elif a2==0:
+    print('0 ga teng')
+else:
+    print("0 dan kichkina")
+
